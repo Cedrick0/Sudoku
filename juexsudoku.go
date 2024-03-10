44f9e0625756605f5645fff7a265ebee37650838
Cedrick0 <123408932+Cedrick0@users.noreply.github.com>
2024-03-10 11:49:47 +0000

    test

--- juexsudoku.go
+++ juexsudoku.go
@@ -1,150 +1,68 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-)
-
 func main() {
 
-	board := parseInput(os.Args[1])
+type SudokuGrid = [][]int
+type MemoizationMap [9][9][10]bool
 
-	printBoard(board)
+var rowsUsed, colsUsed, boxesUsed, boxesUsed MemoizationMap
 
-	if backtrack(&board) {
-		fmt.Println("The Sudoku was solved successfully:")
-		printBoard(board)
-	} else {
-		fmt.Printf("The Sudoku can't be solved.")
-	}
+func initializeMemoizationMaps(grid *SudokuGrid) {
+  for i := 0; i < 9; i++ {
+    for j := 0; j < 9; j++ {
+      num := grid[i][j]
+      if num != 0 {
+        idx := (i/3)*3 + j/3
+        rowsUsed[i][j][num] = true
+        colsUsed[i][j][num] = true
+        boxesUsed[idx][j][num] = true
+      }
+    }
+  }
 }
 
-func backtrack(board *[9][9]int) bool {
-	if !hasEmptyCell(board) {
-		return true
-	}
-	for i := 0; i < 9; i++ {
-		for j := 0; j < 9; j++ {
-			if board[i][j] == 0 {
-				for candidate := 9; candidate >= 1; candidate-- {
-					board[i][j] = candidate
-					if isBoardValid(board) {
-						if backtrack(board) {
-							return true
-						}
-						board[i][j] = 0
-					} else {
-						board[i][j] = 0
-					}
-				}
-				return false
-			}
-		}
-	}
-	return false
+func solveSudoku(grid *SudokuGrid) bool {
+  for i := 0; i < 9; i++ {
+    for j := 0; j < 9; j++ {
+      if grid[i][j] == 0 {
+        for num := 1; num <= 9; num++ {
+          if isValid(i, j, num) {
+            placeNumber(grid, i, j, num)
+
+            if solveSudoku(grid) {
+              return true
+            } else {
+              removeNumber(grid, i, j, num)
+  p          }
+          }
+        }
+        return false
+      }
+    }
+  }
+  return true
 }
 
-func hasEmptyCell(board *[9][9]int) bool {
-	for i := 0; i < 9; i++ {
-		for j := 0; j < 9; j++ {
-			if board[i][j] == 0 {
-				return true
-			}
-		}
-	}
-	return false
+func isValid(row, col, num int) bool {
+  idx := (row / 3) * 3 + col / 3
+  return !rowsUsed[row][num] && !colsUsed[col][num] && !boxesUsed[idx][num]
 }
 
-func isBoardValid(board *[9][9]int) bool {
+func placeNumber(grid *SudokuGrid, row, col, num int) {
+  idx := (row / 3) * 3 + col / 3
 
-	//check duplicates by row
-	for row := 0; row < 9; row++ {
-		counter := [10]int{}
-		for col := 0; col < 9; col++ {
-			counter[board[row][col]]++
-		}
-		if hasDuplicates(counter) {
-			return false
-		}
-	}
-
-	//check duplicates by column
-	for row := 0; row < 9; row++ {
-		counter := [10]int{}
-		for col := 0; col < 9; col++ {
-			counter[board[col][row]]++
-		}
-		if hasDuplicates(counter) {
-			return false
-		}
-	}
-
-	//check 3x3 section
-	for i := 0; i < 9; i += 3 {
-		for j := 0; j < 9; j += 3 {
-			counter := [10]int{}
-			for row := i; row < i+3; row++ {
-				for col := j; col < j+3; col++ {
-					counter[board[row][col]]++
-				}
-				if hasDuplicates(counter) {
-					return false
-				}
-			}
-		}
-	}
-
-	return true
+  grid[row][col] = num
+  rowsUsed[row][num] = true
+  colsUsed[col][num] = true
+  boxesUsed[idx][num] = true
 }
 
-func hasDuplicates(counter [10]int) bool {
-	for i, count := range counter {
-		if i == 0 {
-			continue
-		}
-		if count > 1 {
-			return true
-		}
-	}
-	return false
+func removeNumber(grid *SudokuGrid, row, col, num int) {
+  idx := (row / 3) * 3 + col / 3
+
+  grid[row][col] = 0
+  rowsUsed[row][num] = false
+  colsUsed[col][num] = false
+  boxesUsed[idx][num] = false
 }
-
-func printBoard(board [9][9]int) {
-	fmt.Println("+-------+-------+-------+")
-	for row := 0; row < 9; row++ {
-		fmt.Print("| ")
-		for col := 0; col < 9; col++ {
-			if col == 3 || col == 6 {
-				fmt.Print("| ")
-			}
-			fmt.Printf("%d ", board[row][col])
-			if col == 8 {
-				fmt.Print("|")
-			}
-		}
-		if row == 2 || row == 5 || row == 8 {
-			fmt.Println("\n+-------+-------+-------+")
-		} else {
-			fmt.Println()
-		}
-	}
-}
-
-func parseInput(input string) [9][9]int {
-	board := [9][9]int{}
-	scanner := bufio.NewScanner(strings.NewReader(input))
-
-	scanner.Split(bufio.ScanRunes)
-
-	for row := 0; row < 9; row++ {
-		for col := 0; col < 9; col++ {
-			scanner.Scan()
-			i1, _ := strconv.Atoi(scanner.Text())
-			board[row][col] = i1
-		}
-	}
-	return board
 }
